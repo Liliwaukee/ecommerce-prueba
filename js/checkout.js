c80e1b2c3a69de1188e252a938c5476a1e9a7d4a
Liliwaukee <lilimmvisual@gmail.com>
2018-03-12 02:28:53 -0600

    prueba

---
+++ js/checkout.js
@@ -0,0 +1,9 @@
+function calculateTotal() {
+  //como le hacemos para extraer toda
+  //de cantidades de los elementos
+  //en mi carrito
+  let cartItems = JSON.parse(localStorage.getItem("cart"));
+  console.log(cartItems);
+
+}
+calculateTotal() 
